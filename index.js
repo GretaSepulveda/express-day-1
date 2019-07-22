ae7f0e83012212e834b9c930f6a2f0aedc92c0e2
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-07-21 21:29:06 -0500

    update express project

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-const { users } = require('./state')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+const { users } = require("./state");
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-app.get('/', (req, res) => {
-  res.send('<a href="http://localhost:4000/users">Get Users</a>')
-})
-app.get('/users', (req, res) => {
-  res.json(users)
-})
-app.get('/users/:id', (req, res) => {
-  const found = users.some(user => user._id == req.params.id)
-  if (found){
-    res.send(users.filter(user => user._id == req.params.id))
+app.use(express.urlencoded({ extended: false }));
+
+/* BEGIN - create routes here */
+
+app.get("/", (req, res) => {
+  res.send(`<a href="http://localhost:4000/users">Get Users</a>`);
+});
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+app.get("/users/:id", (req, res) => {
+  const found = users.some(user => user._id == req.params.id);
+  if (found) {
+    res.send(users.filter(user => user._id == req.params.id));
   } else {
-    res.status(404).json({msg: `User id ${req.params.id} not found.`})
+    res.status(400).json({ msg: `User id ${req.params.id} not found.` });
   }
-})
-app.post('/users', (req, res) => {
+});
+
+app.post("/users", (req, res) => {
   const newUser = {
-      id: req.body._id,
-      name: req.body.name,
-      occupation: req.body.occupation,
-      avatar: req.body.avatar
-  }
-  if(!newUser.id) res.status(400).json({ msg: "New users require an id" })
-  users.save(newuser)
-  users.push(newUser)
-  res.json(users)
-})
-app.put('/users/:id', (req, res) => {
-  const updateUser = req.body;
-  users.forEach(user => {
-    if(user._id === parseInt(req.params.id)) {
-      user.name = updateUser.name ? updateUser.name : user.name;
-      user.occupation = updateUser.occupation ? updateUser.occupation : user.occupation;
-      user.avatar = updateUser.avatar ? updateUser.avatar : user.avatar;
-      res.json({ msg: `User updated: ${user}` })
-    }
-  })
-})
-app.delete('/users/:id', (req, res) => {
-  const found = users.some(user => user._id == req.params.id)
-  if (found){
-    res.json({ 
-      msg: `User deleted`, 
-      users: users.filter(user => user._id == req.params.id)
-    })
+    _id: req.body._id,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar
+  };
+  users.push(newUser);
+  res.json(users);
+});
+
+app.put("/users/:id", (req, res) => {
+  const updateUser = {
+    id: req.body._id,
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar
+  };
+  users.push(updateUser);
+  res.json(users);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const found = users.some(user => user._id == req.params.id);
+  if (found) {
+    const userObject = users.filter(user => user._id == req.params.id);
+    console.log(userObject[0]);
+    users.splice(userObject[0]._id - 1, 1);
+    res.send("msg: deleted!");
   } else {
-    res.status(404).json({msg: `User id ${req.params.id} not found.`})
+    res.status(400).json({ msg: `User id ${req.params.id} not found.` });
   }
-})
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+});
+
+/* Finito- create the routes here!!  */
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
